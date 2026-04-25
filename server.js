b11b00ce39501196abfc036fac380b932c337235
Rashminda Aluvihare <rashmindaluvihare@gmail.com>
2026-04-25 11:02:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
